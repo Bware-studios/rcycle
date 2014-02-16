0c27142f61709e3b6c188c6991ba0c149d42a11a
Mikel Izal Azcarate <mikel.izal@unavarra.es>
2014-02-16 23:04:59 +0100

    backup

--- Classes/Options.cpp
+++ Classes/Options.cpp
@@ -13,6 +13,6 @@
 bool Options::debug_draw_physics = false;
 bool Options::debug_draw_alfa = false;
 bool Options::debug_draw_background = true;
-bool Options::debug_draw_outside = true;
+bool Options::debug_draw_outside = false;
 
 
