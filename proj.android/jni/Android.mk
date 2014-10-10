732513db8be8ae72e7231bb280ffcdd6d7a3e0d1
Mikel Izal Azcarate <mikel.izal@unavarra.es>
2014-10-10 09:07:34 +0200

    android project fix

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -21,6 +21,7 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
 	../../Classes/MainMenuScene.cpp \
 	../../Classes/Options.cpp \
 	../../Classes/OptionsScene.cpp \
+	../../Classes/HelpScene.cpp \
 	../../Classes/SceneLoadManager.cpp \
 	../../Classes/StatsScene.cpp \
 	../../Classes/Trash.cpp \
@@ -28,22 +29,30 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
 	../../Classes/WorldFrame.cpp \
 	../../Classes/Scores.cpp \
 	../../Classes/JSONcodec.cpp \
+	../../Classes/Preferences.cpp \
 
 
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
     $(LOCAL_PATH)/../../cocos2d/cocos/editor-support \
+    $(LOCAL_PATH)/../../cocos2d/cocos/ui \
+    $(LOCAL_PATH)/../../cocos2d/cocos \
     $(LOCAL_PATH)/../../cocos2d 
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
 LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocosbuilder_static
-
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_ui_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocostudio_static
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,2d)
+$(call import-module,extensions)
+$(call import-module,ui)
 $(call import-module,audio/android)
 $(call import-module,Box2D)
 $(call import-module,editor-support/cocosbuilder)
+$(call import-module,editor-support/cocostudio)
