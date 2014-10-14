511355cc5fc0006c4b937bcf299146d194be310d
Mikel Izal Azcarate <mikel.izal@unavarra.es>
2014-10-14 09:01:01 +0200

    Net class added

---
+++ Classes/Net.cpp
@@ -0,0 +1,9 @@
+//
+//  Net.cpp
+//  rcycle
+//
+//  Created by Mikel on 14/10/14.
+//
+//
+
+#include <stdio.h>
---
+++ Classes/Net.h
@@ -0,0 +1,8 @@
+//
+//  Net.h
+//  rcycle
+//
+//  Created by Mikel on 14/10/14.
+//
+//
+
