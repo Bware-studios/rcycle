f0122bde6322a9c4eadc028d960d3a7be2680da3
Mikel Izal Azcarate <mikel.izal@unavarra.es>
2015-09-23 08:30:39 +0200

    android compile fix

--- proj.android/jni/Application.mk
+++ proj.android/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi armeabi-v7a 
 #APP_ABI := all
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -DCOCOS2D_DEBUG=1 -std=c++11 -fsigned-char
